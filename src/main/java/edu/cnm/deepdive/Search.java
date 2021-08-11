ceb773abd874a7eacd1a0fa80c9252ed7dc57d8a
Lee Scroggins <lscrggns@yahoo.com>
2021-08-11 11:20:28 -0600

    Initial commit with recursive binary search.

---
+++ src/main/java/edu/cnm/deepdive/Search.java
@@ -0,0 +1,47 @@
+package edu.cnm.deepdive;
+
+public class Search {
+
+  public static int linearSearch(int[] haystack, int needle) {
+    // Is the array sorted ?
+    // What is the range of possible values?
+
+
+    /* 1. Declare a variable */
+    int position = -1;
+
+    for (int i = 0; i < haystack.length; i++) {
+      if (haystack[i] == needle) {
+        position = i;
+        break;
+      }
+    }
+
+
+
+    /* 2. Return the variable */
+    return position;
+  }
+
+  public static int binarySearch(int[] haystack, int needle) {
+    return binarySearch(haystack, needle, 0, haystack.length);
+  }
+
+  private static int binarySearch(int[] haystack, int needle, int start, int end) {
+    int position = ~start;
+    if (end > start) {
+      int mid = (start + end) / 2;
+      int value = haystack[mid];
+      if (value == needle) {
+        position = mid;
+      } else if (value < needle) {
+        position = binarySearch(haystack, needle, mid + 1, end);
+      } else {
+        position = binarySearch(haystack, needle, start, mid);
+      }
+
+    }
+    return position;
+  }
+
+}
